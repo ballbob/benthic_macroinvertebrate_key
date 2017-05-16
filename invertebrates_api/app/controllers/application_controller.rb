3eb66c0611eacf2884bffaf6b74ec7551c2ba3d3
Robert Ball <robert.ball.1989@gmail.com>
2017-05-16 11:00:31 +0100

    Ajax requests now successful! Added responders gem.

--- invertebrates_api/app/controllers/application_controller.rb
+++ invertebrates_api/app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  before_filter :cors_set_access_control_headers
+  skip_before_filter :verify_authenticity_token
+  after_filter :cors_set_access_control_headers
+  respond_to :json
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
