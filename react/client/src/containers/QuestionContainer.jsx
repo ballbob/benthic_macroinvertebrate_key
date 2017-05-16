cb6fa95f253a3f6dd5bf322899c4b74ae3b970f2
Robert Ball <robert.ball.1989@gmail.com>
2017-05-16 09:58:13 +0100

    Corrected syntax error from renamed function.

--- react/client/src/containers/QuestionContainer.jsx
+++ react/client/src/containers/QuestionContainer.jsx
@@ -2,27 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Question from '../components/Question'
 import Family from '../components/Family'
-import Ajax from '../services/Ajax'
+// import Ajax from '../services/Ajax'
 
 class QuestionContainer extends React.Component {
 
   constructor(props){
     super(props)
-    this.setQuestion = this.setQuestion.bind(this)
+    this.setQuestionNumber = this.setQuestionNumber.bind(this)
     this.state = {
-      question: 1
+      questionNumber: 1
     }
   }
 
-  setQuestion(questionNumber){
-    this.setState({question:questionNumber})
+  setQuestionNumber(questionNumber){
+    this.setState({questionNumber:questionNumber})
   }
+
+  // getQuestions(){
+  //   const questionRequest = new Ajax()
+  //   questionRequest.get("") 
+  // }
 
   render(){
     return(
       <div id="question-container">
         <p>Key</p>
-        <Question question1 = "End of abdomen has long, slender breathing tube" answer1="Nepidae (water scorpion)" question2 = "Not as above" answer2="Go to 2"/>
+        <Question question = "End of abdomen has long, slender breathing tube" answer1="Nepidae (water scorpion)" question2 = "Not as above" answer2="Go to 2"/>
       </div>
     )
   }
